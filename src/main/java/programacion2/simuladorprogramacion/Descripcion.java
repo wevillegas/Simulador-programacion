ba45172bfed3df67b778923962014e7a3c5a947d
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2023-08-24 00:08:16 -0300

    cree una clase con  la descripcion de los animales terrestres

---
+++ src/main/java/programacion2/simuladorprogramacion/Descripcion.java
@@ -0,0 +1,60 @@
+package programacion2.simuladorprogramacion;
+
+/**
+ *
+ * @author salu
+ * 
+ */
+
+
+
+public class Descripcion {
+    switch(opcion) {
+        // Agregamos descripcion de los animales terrestres
+        case: "leon":
+            System.out.println("Especie: Panthera leo, clasificacion: mamifero, raza: Aleon africano, alimentacion: carnivoro, color: amarillo, tamaño: 200");
+            break;
+        case: "oso_panda":
+            System.out.println("Especie: Ailuropoda melanoleuca, clasificacion: mamifero, raza: pardo, alimentacion: bambu, color: blanco y negro, tamaño: 1");
+            break;
+        case: "lince":
+            System.out.println("Especie: Lynx spp, clasificacion: mamifero, raza: lynx, alimentacion: carnivoro, color: manchas y rayas,   tamaño: 70");
+            break;
+        case: "chimpance":
+            System.out.println("Especie: primate, clasificacion: mamifero, raza: Pan troglodytes, alimentacion: omniboro, color: negro,   tamaño: 63");
+            break;
+        case: "guepardo":
+            System.out.println("Especie: , clasificacion: mamifero, raza: Acinonyx jubatus, alimentacion: carnivoro, color: amarillo,   tamaño: 1");
+            break;
+         case: "puma":
+            System.out.println("Especie: puma, clasificacion: mamifero, raza: , alimentacion: carnivoro, color: marron,   tamaño: 40");
+            break;
+        case: "tapir":
+            System.out.println("Especie: Tapirus terrestris, clasificacion: mamifero, raza: , alimentacion: herviboro, color: gris,   tamaño: 1");
+            break;           
+        case: "marmota":
+            System.out.println("Especie: roedor, clasificacion: mamifero, raza: , alimentacion: herviboro, color: marron,   tamaño: 40");
+            break;
+        case: "suricata":
+            System.out.println("Especie: Suricata suricatta, clasificacion: mamifero, raza: , alimentacion: carnivoro, color: marron,   tamaño: 25");
+            break;
+        case: "coyote":
+            System.out.println("Especie: Canis latran, clasificacion: mamifero, raza: , alimentacion: omnivoro, color: gris,   tamaño: 90");
+            break;
+        case: "castor":
+            System.out.println("Especie: roedor, clasificacion: mamifero, raza: castor, alimentacion: herviboro, color: marron,   tamaño: 74");
+            break;
+        case: "zorro":
+            System.out.println("Especie: Vulpes vulpes, clasificacion: mamifero, raza: , alimentacion: omnivoro, color: rojizo,   tamaño: 45");
+            break;
+        case: "koala":
+            System.out.println("Especie: marsupial, clasificacion: mamifero, raza: Phascolarctos cinereus, alimentacion: herviboro, color: gris,   tamaño: 60");
+            break;
+        case: "lobo":
+            System.out.println("Especie: Canis lupus, clasificacion: mamifero, raza: , alimentacion: carnivoro, color: gris,   tamaño: 1");
+            break;
+        case: "jirafa":
+            System.out.println("Especie: Giraffa camelopardalis, clasificacion: mamifero, raza: , alimentacion: herviboro, color: amarillo,   tamaño: 4");
+            break;
+    }
+}
