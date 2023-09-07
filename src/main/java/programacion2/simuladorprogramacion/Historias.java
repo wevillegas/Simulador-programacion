73be60217870f38eb19ecd365577873a454283ce
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2023-09-07 11:21:20 -0300

    Agregamos las historias de agua, tierra y cielo a la clase historias

--- src/main/java/programacion2/simuladorprogramacion/Historias.java
+++ src/main/java/programacion2/simuladorprogramacion/Historias.java
@@ -17,11 +17,11 @@ public class Historias {
 
     public String MostrarHistoriaAgua() {
         String[] historiasAgua = {
-            "Historia agua 1",
-            "Historia agua 2",
-            "Historia agua 3",
-            "Historia agua 4",
-            "Historia agua 5"
+            "Bajo la luna llena, las olas rompen suavemente en la playa desierta. El mar susurra historias antiguas, llenando de misterio la noche tranquila.",
+            "Una pareja de ancianos camina de la mano a lo largo del paseo marítimo.",
+            "Una madre lleva a su hijo a ver el océano por primera vez. El niño observa con ojos asombrados, sintiendo la inmensidad y la eternidad del mar.",
+            "Un pescador lanza su red al agua, esperando con paciencia. El mar le enseña que la recompensa llega a quienes perseveran, con la abundancia de sus aguas.",
+            "En la oscuridad de la noche, las luciérnagas del mar iluminan las aguas. Su resplandor crea un espectáculo mágico que deja a los observadores maravillados por la belleza oculta del océano."
         };
 
         int indice = random.nextInt(historiasAgua.length);
@@ -30,11 +30,16 @@ public class Historias {
 
     public String MostrarHistoriaTierra() {
         String[] historiasTierra = {
-            "Historia tierra 1",
-            "Historia tierra 2",
-            "Historia tierra 3",
-            "Historia tierra 4",
-            "Historia tierra 5"
+            "Un grupo de amigos excursiona por las montañas, enfrentando desafíos naturales. Se encuentran tal animal...",
+            "Mariposas y abejas comparten néctar de flores. Su danza simbiótica destaca la interdependencia en la vida terrestre y el papel vital de cada ser en el ecosistema.",
+            "Una pareja camina de la mano por un sendero, encontrando serenidad en la naturaleza. La vida terrestre ofrece refugio y sanación a quienes buscan la tranquilidad.",
+            "Un excursionista observa un río serpenteante desde lo alto de una montaña. La vida terrestre fluye con un ritmo eterno que nos recuerda la constancia de la naturaleza.",
+            "Un leñador tala un árbol con respeto y gratitud. La vida terrestre nos enseña que, aunque tomemos de la naturaleza, debemos hacerlo con consideración.",
+            "Una familia se relaja junto a un río, escuchando el murmullo del agua. La vida terrestre fluye como un río, siempre cambiante y siempre en movimiento.",
+            "En un campo de lavanda, las abejas zumban en busca de néctar. La vida terrestre es un concierto de sonidos y aromas que celebra la diversidad de la Tierra.",
+            "Un grupo de amigos monta una tienda de campaña en la montaña. La vida terrestre es una invitación a la aventura y a conectar con la esencia de la naturaleza.",
+            "Un corredor se sumerge en un bosque en la madrugada, sintiendo la frescura del aire. La vida terrestre es un recordatorio de la vitalidad y la belleza de la naturaleza.",
+            "Un guía turístico comparte historias sobre la vida silvestre. La vida terrestre nos conecta con el pasado y el futuro de nuestro planeta."
         };
 
         int indice = random.nextInt(historiasTierra.length);
@@ -43,11 +48,11 @@ public class Historias {
 
     public String MostrarHistoriaAire() {
         String[] historiasAire = {
-            "Historia aire 1",
-            "Historia aire 2",
-            "Historia aire 3",
-            "Historia aire 4",
-            "Historia aire 5"
+            "En el cielo matinal, los pájaros despiertan con melodías que alegran al mundo. Sus cantos, como rayos de sol, iluminan nuestros corazones.",
+            "Al atardecer, los pájaros forman figuras en el cielo con sus vuelos sincronizados, pintando un cuadro efímero de belleza natural.",
+            "En una mañana brumosa, los pájaros emergen de la niebla como susurros alados. El cielo se llena de misterio y maravilla cuando revelan sus siluetas.",
+            "Un sol radiante ilumina el cielo, mientras los pájaros migratorios cruzan continentes en un ballet celestial. Sus viajes reflejan la persistencia y la esperanza en medio de vastos horizontes.",
+            "En un rincón del cielo, un águila majestuosa surca los vientos con su presa en garras. La naturaleza se muestra implacable, pero también sublime, en esta danza de cazador y presa."
         };
 
         int indice = random.nextInt(historiasAire.length);
